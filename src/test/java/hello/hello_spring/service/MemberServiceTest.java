a2a4d1ed07d36604e01c6f235109862a176a76cc
madogkwon <a93081077a@gmail.com>
2024-10-23 19:45:05 +0900

    first commit

---
+++ src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -0,0 +1,22 @@
+package hello.hello_spring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+  MemberService memberService = new MemberService();
+
+  @Test
+  void join() {
+  }
+
+  @Test
+  void findMembers() {
+  }
+
+  @Test
+  void findOne() {
+  }
+}
